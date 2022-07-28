3bd8bf71bdc5d7c67ad2c1d00cf40e479de4c03d
McGrady00H <281130306@qq.com>
2022-07-28 16:39:14 +0800

    add MNW and ZVC

---
+++ src/prod/symbols/x10_MNW.ts
@@ -0,0 +1,3 @@
+export enum TokenSymbol {
+    MNW = "MNW",
+}
--- src/prod/tokens/x999_free.ts
+++ src/prod/tokens/x999_free.ts
@@ -1,4 +1,4 @@
-import { TokenSymbol } from "../symbols/x9_free";
+import { TokenSymbol } from "../symbols/x999_free";
 import { ChainId } from "../chainId";
 
 export interface TokenInfo {
