2b649b4124572f70a91ddc4ae1a33ef88d498141
McGrady00H <281130306@qq.com>
2023-03-20 12:28:35 +0800

    update

--- src/prod/tokens/x34_slToken.ts
+++ src/prod/tokens/x34_slToken.ts
@@ -16,9 +16,13 @@ export const tokens: TokenInfo[] = [
         name: 'Shared-liquidity USDT',
         symbol: TokenSymbol.SLUSDT,
         icon: baseURL + 'slusdt.png',
-        chains: [ChainId.BSC],
+        chains: [ChainId.BSC, ChainId.Arbitrum],
         contracts: {
             [ChainId.BSC]: {
+                address: '0x65CD2E7d7BAcDaC3AA9dAe68fb5d548dfE1fEFB5',
+                decimal: 18,
+            },
+            [ChainId.Arbitrum]: {
                 address: '0x65CD2E7d7BAcDaC3AA9dAe68fb5d548dfE1fEFB5',
                 decimal: 18,
             },
@@ -28,9 +32,13 @@ export const tokens: TokenInfo[] = [
         name: 'Shared-liquidity USDC',
         symbol: TokenSymbol.SLUSDC,
         icon: baseURL + 'slusdc.png',
-        chains: [ChainId.BSC],
+        chains: [ChainId.BSC, ChainId.Arbitrum],
         contracts: {
             [ChainId.BSC]: {
+                address: '0x55a26773A8ba7FA9a8242B43e14a69488e6D2B05',
+                decimal: 18,
+            },
+            [ChainId.Arbitrum]: {
                 address: '0x55a26773A8ba7FA9a8242B43e14a69488e6D2B05',
                 decimal: 18,
             },
@@ -40,9 +48,13 @@ export const tokens: TokenInfo[] = [
         name: 'Shared-liquidity BTC',
         symbol: TokenSymbol.SLBTC,
         icon: baseURL + 'slbtc.png',
-        chains: [ChainId.BSC],
+        chains: [ChainId.BSC, ChainId.Arbitrum],
         contracts: {
             [ChainId.BSC]: {
+                address: '0xe04B30F233E3Bc96Da2401EE146a94f1C7C54917',
+                decimal: 18,
+            },
+            [ChainId.Arbitrum]: {
                 address: '0xe04B30F233E3Bc96Da2401EE146a94f1C7C54917',
                 decimal: 18,
             },
@@ -55,6 +67,10 @@ export const tokens: TokenInfo[] = [
         chains: [ChainId.BSC],
         contracts: {
             [ChainId.BSC]: {
+                address: '0xdF6964C7ccfaBaACe7C3f918B795fA4a894589C8',
+                decimal: 18,
+            },
+            [ChainId.Arbitrum]: {
                 address: '0xdF6964C7ccfaBaACe7C3f918B795fA4a894589C8',
                 decimal: 18,
             },
