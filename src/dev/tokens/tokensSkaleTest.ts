342bc093b2217ab6db0c23d516f21e6cf1ecf4b3
McGrady00H <281130306@qq.com>
2023-08-30 14:31:58 +0800

    update

--- src/dev/tokens/tokensSkaleTest.ts
+++ src/dev/tokens/tokensSkaleTest.ts
@@ -12,28 +12,28 @@ export interface TokenInfo {
 const baseURL = 'https://izumi-finance.oss-ap-southeast-1.aliyuncs.com/tokens/'
 
 export const tokens: TokenInfo[] = [
-    {
-        name: "sFUEL",
-        symbol: TokenSymbol.SFUEL,
-        chains: [ChainId.SkaleTest],
-        icon: baseURL + 'sfuel.png',
-        contracts: {
-            [ChainId.SkaleTest]: {
-                address: '0x67A1f4A939b477A6b7c5BF94D97E45dE87E608eF', // wsfuel
-                decimal: 18,
-            },
-        },
-    },
-    {
-        name: "Wrapped sFUEL",
-        symbol: TokenSymbol.WSFUEL,
-        chains: [ChainId.SkaleTest],
-        icon: baseURL + 'sfuel.png',
-        contracts: {
-            [ChainId.SkaleTest]: {
-                address: '0x67A1f4A939b477A6b7c5BF94D97E45dE87E608eF', 
-                decimal: 18,
-            },
-        },
-    },
+    // {
+    //     name: "sFUEL",
+    //     symbol: TokenSymbol.SFUEL,
+    //     chains: [ChainId.SkaleTest],
+    //     icon: baseURL + 'sfuel.png',
+    //     contracts: {
+    //         [ChainId.SkaleTest]: {
+    //             address: '0x67A1f4A939b477A6b7c5BF94D97E45dE87E608eF', // wsfuel
+    //             decimal: 18,
+    //         },
+    //     },
+    // },
+    // {
+    //     name: "Wrapped sFUEL",
+    //     symbol: TokenSymbol.WSFUEL,
+    //     chains: [ChainId.SkaleTest],
+    //     icon: baseURL + 'sfuel.png',
+    //     contracts: {
+    //         [ChainId.SkaleTest]: {
+    //             address: '0x67A1f4A939b477A6b7c5BF94D97E45dE87E608eF', 
+    //             decimal: 18,
+    //         },
+    //     },
+    // },
 ]
