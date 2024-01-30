8d1dc4c553e54ff218fb370b5569c4091e95968d
McGrady00H <281130306@qq.com>
2024-01-30 17:43:42 +0800

    update

--- src/prod/symbols/tokenZeta.ts
+++ src/prod/symbols/tokenZeta.ts
@@ -1,4 +1,11 @@
 export enum TokenSymbol {
     ZETAZeta = 'ZETA',
     WZETAZeta = 'WZETA',
+    ETHETHZeta = 'ETH.ETH',
+    BNBBSCZeta = 'BNB.BSC',
+    BTCBTCZeta = 'BTC.BTC',
+    USDTBSCZeta = 'USDT.BSC',
+    USDTETHZeta = 'USDT.ETH',
+    USDCBSCZeta = 'USDC.BSC',
+    USDCETHZeta = 'USDC.ETH',
 }
