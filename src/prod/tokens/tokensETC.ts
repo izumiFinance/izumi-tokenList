5a6c73ea3c92a5901bbe507849f5ea611fc23576
McGrady00H <281130306@qq.com>
2024-04-22 17:25:25 +0800

    update

--- src/prod/tokens/tokensETC.ts
+++ src/prod/tokens/tokensETC.ts
@@ -12,28 +12,28 @@ export interface TokenInfo {
 const baseURL = 'https://izumi-finance.oss-ap-southeast-1.aliyuncs.com/tokens/'
 
 export const tokens: TokenInfo[] = [
-    {
-        name: "Ether Classic",
-        symbol: TokenSymbol.ETCETC,
-        icon: baseURL + 'etc.png',
-        chains: [ChainId.ETC],
-        contracts: {
-            [ChainId.ETC]: {
-                address: '0x82A618305706B14e7bcf2592D4B9324A366b6dAd', 
-                decimal: 18,
-            },
-        },
-    },
-    {
-        name: "Wrapped ETC",
-        symbol: TokenSymbol.WETCETC,
-        icon: baseURL + 'etc.png',
-        chains: [ChainId.ETC],
-        contracts: {
-            [ChainId.ETC]: {
-                address: '0x82A618305706B14e7bcf2592D4B9324A366b6dAd', 
-                decimal: 18,
-            },
-        },
-    },
+    // {
+    //     name: "Ether Classic",
+    //     symbol: TokenSymbol.ETCETC,
+    //     icon: baseURL + 'etc.png',
+    //     chains: [ChainId.ETC],
+    //     contracts: {
+    //         [ChainId.ETC]: {
+    //             address: '0x82A618305706B14e7bcf2592D4B9324A366b6dAd', 
+    //             decimal: 18,
+    //         },
+    //     },
+    // },
+    // {
+    //     name: "Wrapped ETC",
+    //     symbol: TokenSymbol.WETCETC,
+    //     icon: baseURL + 'etc.png',
+    //     chains: [ChainId.ETC],
+    //     contracts: {
+    //         [ChainId.ETC]: {
+    //             address: '0x82A618305706B14e7bcf2592D4B9324A366b6dAd', 
+    //             decimal: 18,
+    //         },
+    //     },
+    // },
 ]
