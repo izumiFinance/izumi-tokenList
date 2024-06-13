722e3b3156439893e555c92b4b06b433b5b594f1
McGrady00H <281130306@qq.com>
2024-06-13 13:27:20 +0800

    update

--- src/prod/symbols/tokenCyber.ts
+++ src/prod/symbols/tokenCyber.ts
@@ -1,2 +1,3 @@
 export enum TokenSymbol {
+    CYBERCyber = 'CYBER',
 }
--- src/prod/tokens/tokensCyber.ts
+++ src/prod/tokens/tokensCyber.ts
@@ -11,4 +11,17 @@ export interface TokenInfo {
 
 const baseURL = 'https://izumi-finance.oss-ap-southeast-1.aliyuncs.com/tokens/'
 
-export const tokens: TokenInfo[] = []
+export const tokens: TokenInfo[] = [
+    {
+        name: "Cyber Token",
+        symbol: TokenSymbol.CYBERCyber,
+        chains: [ChainId.Cyber],
+        icon: baseURL + 'cyber.png',
+        contracts: {
+            [ChainId.Cyber]: {
+                address: '0x14778860E937f509e651192a90589dE711Fb88a9',
+                decimal: 18, 
+            },
+        },
+    },
+]
