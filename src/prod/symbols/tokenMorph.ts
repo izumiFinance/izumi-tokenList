989a4ffca178a72076468f4f97f7dd6915574157
SonGokuMega <281130306@qq.com>
2024-10-30 20:28:19 +0800

    update

--- src/prod/symbols/tokenMorph.ts
+++ src/prod/symbols/tokenMorph.ts
@@ -1,2 +1,4 @@
-export enum TokenSymbol {}
+export enum TokenSymbol {
+    KOLMorph = 'KOL',
+}
 
--- src/prod/tokens/tokensMorph.ts
+++ src/prod/tokens/tokensMorph.ts
@@ -12,4 +12,16 @@ export interface TokenInfo {
 const baseURL = 'https://izumi-finance.oss-ap-southeast-1.aliyuncs.com/tokens/'
 
 export const tokens: TokenInfo[] = [
+    {
+        name: "Morph Koala",
+        symbol: TokenSymbol.KOLMorph,
+        icon: baseURL + 'koala.png',
+        chains: [ChainId.Morph],
+        contracts: {
+            [ChainId.Morph]: {
+                address: '0xD8b8d868e994df5D4Ba8bD599eadBc21598a732E',
+                decimal: 18,
+            },
+        },
+    },
 ]
