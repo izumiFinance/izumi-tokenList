78bbf03a1b8b9c982dcd16b93e917f8134b84578
SonGokuMega <281130306@qq.com>
2025-03-05 16:43:06 +0800

    update

--- src/prod/symbols/tokenHemi.ts
+++ src/prod/symbols/tokenHemi.ts
@@ -1,2 +1,3 @@
 export enum TokenSymbol {
+    VUSDHemi = 'VUSD',
 }
---
+++ src/prod/tokens/tokensHemi.ts
@@ -0,0 +1,27 @@
+import { TokenSymbol } from "../symbols/tokenHemi";
+import { ChainId } from "../chainId";
+
+export interface TokenInfo {
+    chains: number[];
+    name: string;
+    symbol: TokenSymbol,
+    icon: string;
+    contracts: Partial<Record<ChainId, { address: string; decimal: number }>>;
+}
+
+const baseURL = 'https://izumi-finance.oss-ap-southeast-1.aliyuncs.com/tokens/'
+
+export const tokens: TokenInfo[] = [
+    {
+        name: "VUSD Token",
+        symbol: TokenSymbol.VUSDHemi,
+        icon: baseURL + 'vusd.png',
+        chains: [ChainId.Hemi],
+        contracts: {
+            [ChainId.Hemi]: {
+                address: '0x7A06C4AeF988e7925575C50261297a946aD204A8',
+                decimal: 18,
+            },
+        },
+    },
+]
