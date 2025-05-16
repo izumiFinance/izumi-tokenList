ffba38b743de039d2ca35eb376e437f6c42002bb
SonGokuMega <281130306@qq.com>
2025-05-16 16:11:49 +0800

    update

--- src/dev/tokens/tokensMonadTest.ts
+++ src/dev/tokens/tokensMonadTest.ts
@@ -173,7 +173,7 @@ export const tokens: TokenInfo[] = [
         name: "aPriori Monad LST",
         symbol: TokenSymbol.APRMONMonad,
         chains: [ChainId.MonadTest],
-        icon: baseURL + 'aprmon.svg',
+        icon: baseURL + 'aprmon.png',
         contracts: {
             [ChainId.MonadTest]: {
                 address: '0xb2f82D0f38dc453D596Ad40A37799446Cc89274A',
